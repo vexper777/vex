1a6b13c36c86a669d9702698a7ef2f4ac3155bba
vexper777 <pietropanebianco4@gmail.com>
2025-12-25 15:08:13 +0100

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -2,8 +2,8 @@ let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
 m.reply('𝒗𝒆𝒅𝒐 𝒐𝒇𝒇 𝒄𝒐𝒔𝒊 𝒏𝒐𝒏 𝒎𝒊 𝒓𝒐𝒎𝒑𝒆𝒕𝒆 𝒊 𝒄𝒐𝒈𝒍𝒊𝒐𝒏𝒊')
 }
-handler.help = ['silent']
+handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = /^silent|bangp$/i
+handler.command = /^.silent|bangp$/i
 handler.rowner = true
 export default handler
