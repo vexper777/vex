f47b99a2e30cc7a1a9364d939edad437cf8a3648
vexper777 <pietropanebianco4@gmail.com>
2026-01-12 00:09:56 +0100

    Create infame-dox.js

---
+++ plugins/infame-dox.js
@@ -0,0 +1,203 @@
+let handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { text: `Dox di Ali El Mir aka Negro (ex admin other side)
+
+
+City:Perugia,Umbria, 
+Instagram: 
+https://instagram.com/ali._06127
+Instagram2:
+https://www.instagram.com/aliiii.x2
+Numero di telefono:
++39 389 545 9079
+email:seisolounrandom@gmail.com
+fb:https://www.facebook.com/share/1GFCa5Nn7D/?mibextid=wwXIfr
+
+BREACH:
+
+  },
+  "status": "found",
+  "results": {
+    "query": "seisolounrandom@gmail.com",
+    "email_osint": {
+      "registered_on": [
+        "Google",
+        "GitHub",
+        "Amazon",
+        "IMVU",
+        "AutoDS"
+      ],
+      "scan_details": [
+        {
+          "service": "Google",
+          "status": "FOUND"
+        },
+        {
+          "service": "GitHub",
+          "status": "FOUND"
+        },
+        {
+          "service": "Amazon",
+          "status": "FOUND",
+          "message": "Account FOUND.",
+          "data": null
+        },
+        {
+          "service": "IMVU",
+          "status": "FOUND",
+          "message": "Account FOUND"
+        },
+        {
+          "service": "AutoDS",
+          "status": "FOUND",
+          "data": {}
+        }
+      ]
+    }
+  }
+}
+
+Family Members 
+
+Youssef El Mir
+fb:Youssef El Mir
+https://www.facebook.com/share/1H7nMV1FFs/?mibextid=wwXIfr
+fb2:https://www.facebook.com/share/1KgYQzKukq/?mibextid=wwXIfr
+
+
+
+
+Hamza El Mir
+fb:https://www.facebook.com/share/17hztBdPJH/?mibextid=wwXIfr
+
+
+
+Cauter El Mir 
+fb:https://www.facebook.com/share/1J8z5SBdFR/?mibextid=wwXIfr
+
+Email+Breach
+{
+  "success": true,
+  "data": {
+    "took": 116,
+    "size": 1,
+    "results": {
+      "0950_DAILYQUIZ_ME_12M_EDU_012021": [
+        {
+          "username": "cauterelmir",
+          "email": "Cauterelmir@live.it",
+          "lastip": "79.13.216.37",
+          "password": "hassanbari95",
+          "_domain": "live.it",
+          "uid": "14379041",
+          "created": "2019-02-18 15:45:47\r"
+        }
+      ]
+    }
+  },
+  "timestamp": "2026-01-07T18:31:34.629Z"
+}
+{
+  "email": "cauterelmir@live.it",
+  "breaches": [
+    {
+      "name": "cBANK",
+      "year": 2022,
+      "credentials": [
+        {
+          "email": "cauterelmir@live.it",
+          "password": "hassanbari95"
+        },
+        {
+          "email": "Cauterelmir@live.it",
+          "password": "themoon6"
+        },
+        {
+          "email": "cauterelmir@live.it",
+          "password": "themoon6"
+        }
+      ]
+    },
+    {
+      "name": "BreachedForum Combolists",
+      "source": "breached.vc",
+      "date_downloaded": "2023-02-22",
+      "credentials": [
+        {
+          "email": "cauterelmir@live.it",
+          "password": "hassanbari95"
+        }
+      ]
+    },
+    {
+      "name": "LeakBase Collection",
+      "source": "leakbase.cc",
+      "date_downloaded": "2023-01",
+      "credentials": [
+        {
+          "email": "cauterelmir@live.it",
+          "password": "hassanbari95"
+        }
+      ]
+    },
+    {
+      "name": "Daily Quiz (Victorin)",
+      "year": 2021,
+      "credentials": [
+        {
+          "email": "Cauterelmir@live.it",
+          "password": "hassanbari95",
+          "registration_date": "2019-02-18 15:45:47",
+          "ip": "79.13.216.37",
+          "nickname": "cauterelmir"
+        }
+      ]
+    },
+    {
+      "name": "Cloudata",
+      "date_collected": "2023-05-18",
+      "credentials": [
+        {
+          "email": "cauterelmir@live.it",
+          "password": "hassanbari95"
+        },
+        {
+          "email": "cauterelmir@live.it",
+          "password": "themoon6"
+        }
+      ]
+    }
+  ]
+}
+
+Abdelali El Mir
+FB:https://www.facebook.com/share/1Am3SXoJmy/?mibextid=wwXIfr
+Numero di telefono:393279058654
+
+
+Omar El Mir
+FB:https://www.facebook.com/share/1NpjWMsLVT/?mibextid=wwXIfr
+
+
+Ayoub El Mir
+FB:https://www.facebook.com/share/14VCzAayqyA/?mibextid=wwXIfr
+
+
+Rachid El Mir
+FB:https://www.facebook.com/share/1A3vv95pw7/?mibextid=wwXIfr
+
+Kamal El Mir
+FB:https://www.facebook.com/share/14M1MBxUbzy/?mibextid=wwXIfr
+
+Hamza El Mir
+FB:https://www.facebook.com/share/18EB4fk9VP/?mibextid=wwXIfr
+
+Yosef El Mir
+FB:https://www.facebook.com/share/1KgYQzKukq/?mibextid=wwXIfr
+
+Allal El Mir
+FB:https://www.facebook.com/share/14NAxLGohqn/?mibextid=wwXIfr` }, { quoted: m })
+}
+
+handler.command = ['npm👍🏻']
+
+export default handler
