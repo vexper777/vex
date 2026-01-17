d3aa59273e8254d45d5b15dee2d4c4d85dcfbf34
vexper777 <pietropanebianco4@gmail.com>
2026-01-17 22:51:56 +0100

    Update owner-banchat.js

---
+++ plugins/owner-banchat.js
@@ -0,0 +1,10 @@
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = true
+m.reply('𝐌𝐨𝐝𝐚𝐥𝐢𝐭à 𝐀𝐅𝐊 𝐚𝐭𝐭𝐢𝐯𝐚𝐭𝐚 ✓')
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^bonoir$/i
+handler.rowner = true
+export default handler
+
