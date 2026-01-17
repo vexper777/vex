1e98c6e1b07641ba298e245701e094adfdb2de70
dthbot <blebledarius@gmail.com>
2026-01-17 23:29:53 +0100

    Delete OWNER-unbanchat.js

--- plugins/OWNER-unbanchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('𝕀𝐋 𝐁𝕆𝐓 𝕊𝕀 𝚵 𝕊𝐕𝚵𝐆𝐋𝕀𝚲𝐓Ꮻ 🔔')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-handler.rowner = true
-export default handler
